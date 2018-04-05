baef3f9c81de472ea63128632355894585bc833a
Brendan Thomas <bt331609@gmail.com>
2018-04-05 17:31:32 -0400

    add AuthorsList component to /authors

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/components/Login';
 import Logout from '@/components/Logout';
+import AuthorsList from '@/components/Authors/List'
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/logout',
       name: 'Logout',
       component: Logout
+    },
+    {
+      path: '/authors',
+      name: 'Authors',
+      component: AuthorsList
     }
   ],
 });
